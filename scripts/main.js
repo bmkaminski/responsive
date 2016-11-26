2a846a63746e920ce699d38f0e8952c0ef6d4b54
bmkaminski <Brooke@Brookes-MacBook-Pro.local>
2016-11-26 14:07:04 -0800

    moved media queries to separate file

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,32 @@ var $btnMobile = $('.btn-mobile');
 
 $btnMobile.click(function(){
 	$body.toggleClass('show');
+
+      var el = $(this);
+  if (el.text() == el.data('text-swap')) {
+    el.text(el.data('text-original'));
+  } else {
+    el.data('text-original', el.text());
+    el.text(el.data('text-swap'));
+  }
+    
 });
 
 // smooth scroll
+
+
+
+// mobile text
+
+//var $text = $('.review');
+//
+//if ($text <= 'width: 480px'){
+//    $text.html('read more...');
+//}
+
+
+ $(window).resize(function() {
+  if ($(window).width() <= 480) {
+     $('.pp p').html('click to read more...');
+  }
+});

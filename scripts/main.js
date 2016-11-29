f2e4881b38cc15a28cac3dc7a3439d53174fdec3
bmkaminski <Brooke@brookes-mbp.wifi.bcit.ca>
2016-11-29 14:20:10 -0800

    changed media query backgrounds, fixed tablet nav

--- scripts/main.js
+++ scripts/main.js
@@ -50,3 +50,38 @@ $('a[href^="#link"]').click(function(event){
       }
       
     });
+
+
+/* ---------- subscribe ---------- */
+
+$('#submit').click(function(){
+    $('#subscribe').text('Thanks for subscribing!');
+});
+
+
+
+//var $btn = $('button');
+//var $nav = $('nav');
+//var $navUl = $('nav > ul');
+//
+//$btn.click(function(){
+//	$navUl.toggleClass('open');	
+//});
+//
+//// The following JS modified from 
+//// JS found at this per: http://codepen.io/laviperchik/pen/dlcBt
+//$.fn.accessibleDropDown = function () {
+//    
+//	var el = $(this);
+//
+//    /* Make dropdown menus keyboard accessible */
+//
+//    el.find('a').focus(function() {
+//        $(this).parents("li").addClass("hover");
+//    }).blur(function() {
+//        $(this).parents("li").removeClass("hover");
+//    });
+//
+//}
+//
+//$nav.accessibleDropDown();
